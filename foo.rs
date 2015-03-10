b9dad9071d48dfe73b424a12998d1c860a27676e
Bruno Windels <bruno.windels@gmail.com>
2015-03-10 02:55:20 +0100

    fix error

--- foo.rs
+++ foo.rs
@@ -45,7 +45,6 @@ pub extern fn token_collection_len(pimpl: *mut TokenCollection) -> libc::uint64_
 
 #[no_mangle]
 pub extern fn token_collection_destroy(pimpl: *mut TokenCollection) {
-	let str = c_str_to_slice(c_str);
 
 	let _drop_me = unsafe { transmute(box TokenCollection::new(str)) };
 }
