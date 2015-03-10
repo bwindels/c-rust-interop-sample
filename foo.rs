df5a9c7c6c010cae6f96ef4afbaca42847adeda5
Bruno Windels <bruno.windels@gmail.com>
2015-03-10 02:29:26 +0100

    try to model an api with memory allocation and pointer sharing between rust and c

--- foo.rs
+++ foo.rs
@@ -1,5 +1,51 @@
-#![crate_type = "staticlib"]
+use libc::c_char;
+use std::ffi;
+use std::str;
+use std::vec;
+
+struct TokenCollection {
+	parts: Vec<&str>,
+	name: &str
+}
+
+impl TokenCollection {
+	fn new(str: &str) -> &TokenCollection {
+		TokenCollection {
+			parts: str.split(|c| c.is_whitespace()).collect()
+		}
+	}
+
+	fn len(&self) -> usize {
+		self.parts.len()
+	}
+}
+
+// impl Drop for TokenCollection {
+// 	fn drop(&self) {
+// 		println!("drop it like it's hot");
+// 	}
+// }
+
+fn c_str_to_slice(c_str: *mut c_char) -> &str {
+	let c_bytes = unsafe { ffi::c_str_to_bytes(&c_str); };
+	std::from_utf8(c_bytes).unwrap()
+}
+
 #[no_mangle]
-pub extern "C" fn foo() {
-	println!("hello from rust");
+pub extern fn token_collection_create(c_str: *mut c_char) -> *mut TokenCollection {
+	let str = c_str_to_slice(c_str);
+
+	unsafe { transmute(box TokenCollection::new(str)) }
+}
+
+#[no_mangle]
+pub extern fn token_collection_len(pimpl: *mut TokenCollection) -> libc::uint64_t {
+	pimpl.len()
+}
+
+#[no_mangle]
+pub extern fn token_collection_destroy(pimpl: *mut TokenCollection) {
+	let str = c_str_to_slice(c_str);
+
+	let _drop_me = unsafe { transmute(box TokenCollection::new(str)) };
 }
--- use_foo.c
+++ use_foo.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-void foo();
+struct TokenCollection;
+
+TokenCollection* token_collection_create(const char*);
+uint64_t token_collection_len(TokenCollection*);
+void token_collection_destroy(TokenCollection*);
 
 int main() {
-	printf("hello from c\n");
-	foo();
+	TokenCollection* col = token_collection_create("this is a sentence");
+	printf("%d tokens", token_collection_len(col));
+	token_collection_destroy(col);
 	return 0;
 }
